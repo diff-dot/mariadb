6281d1772953a0d1385c681f736b2d600359584c
Hyogeun Wang <w@endless9.com>
2021-08-22 05:52:06 +0900

    코코드 정리

--- src/MariadbRepository.ts
+++ src/MariadbRepository.ts
@@ -11,23 +11,6 @@ import { WriteResult } from './type/WriteResult';
  * Entity 를 Mariadb 에서 관리하기 위한 클래스
  */
 export abstract class MariadbRepository extends Repository {
-  protected async addEntity(entity: Entity, options: { connection?: PoolConnection } = {}): Promise<WriteResult> {
-    const entityOptions = getMariadbEntityOptions(entity);
-    const entitySql = new EntityWriteSql(entity);
-
-    const connection = options.connection || (await MariadbClient.instance(entityOptions.host).connection());
-
-    try {
-      const res = await connection.query(
-        `INSERT INTO ${entityOptions.db}.${entityOptions.table}(${entitySql.columns()}) VALUES(${entitySql.valuesPlaceholder()})`,
-        entitySql.values()
-      );
-      return res;
-    } finally {
-      if (!options.connection) await connection.release();
-    }
-  }
-
   protected async entity<T extends Entity, K extends keyof T>(args: {
     entityConstructor: { new (...args: unknown[]): T };
     where: Partial<T>;
@@ -55,6 +38,23 @@ export abstract class MariadbRepository extends Repository {
       return plainToClass(entityConstructor, res[0] as Record<string, unknown>);
     } finally {
       if (!args.connection) await connections.release();
+    }
+  }
+
+  protected async addEntity(entity: Entity, options: { connection?: PoolConnection } = {}): Promise<WriteResult> {
+    const entityOptions = getMariadbEntityOptions(entity);
+    const entitySql = new EntityWriteSql(entity);
+
+    const connection = options.connection || (await MariadbClient.instance(entityOptions.host).connection());
+
+    try {
+      const res = await connection.query(
+        `INSERT INTO ${entityOptions.db}.${entityOptions.table}(${entitySql.columns()}) VALUES(${entitySql.valuesPlaceholder()})`,
+        entitySql.values()
+      );
+      return res;
+    } finally {
+      if (!options.connection) await connection.release();
     }
   }
 
